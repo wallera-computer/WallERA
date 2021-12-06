776f73b5b9e8fe2717d6f5655d781f80dbeca94b
Gianguido Sorà <me@gsora.xyz>
2021-12-06 17:07:07 +0100

    improvement(cosmos): implement partially getAddress call

--- apps/cosmos/cosmos.go
+++ apps/cosmos/cosmos.go
@@ -3,6 +3,7 @@ package cosmos
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"log"
 )
@@ -16,9 +17,25 @@ const (
 	claGetVersion       command = 0x00
 	claSignSecp256K1    command = 0x02
 	claGetAddrSecp256K1 command = 0x04
+
+	// TODO: remove this
+	testAddrBytes = "994bc0e6262f6d129aab9911074d836e60c3a8f8"
 )
 
-var commandCodeOK = [2]byte{0x90, 0x00}
+func testBytes() []byte {
+	d, err := hex.DecodeString(testAddrBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return d
+}
+
+var (
+	commandCodeOK         = [2]byte{0x90, 0x00}
+	commandErrEmptyBuffer = [2]byte{0x69, 0x82}
+	commandErrWrongLength = [2]byte{0x67, 0x82}
+)
 
 type Cosmos struct {
 }
@@ -45,7 +62,7 @@ func (c *Cosmos) Handle(cmd byte, data []byte) (response []byte, code [2]byte, e
 	log.Println("handling command", command(cmd).String())
 	switch cmd {
 	case byte(claGetVersion):
-		return c.handleGetVersion(data)
+		return c.handleGetVersion()
 	case byte(claSignSecp256K1):
 		return c.handleSignSecp256K1(data)
 	case byte(claGetAddrSecp256K1):
@@ -55,13 +72,6 @@ func (c *Cosmos) Handle(cmd byte, data []byte) (response []byte, code [2]byte, e
 		return nil, [2]byte{}, fmt.Errorf("command not found")
 	}
 }
-
-/*
-   test_mode: response[0] !== 0,
-   version: "" + response[1] + "." + response[2] + "." + response[3],
-   device_locked: response[4] === 1,
-   major: response[1],
-*/
 
 type version struct {
 	Major uint8
@@ -82,13 +92,13 @@ func (g getVersionResponse) Marshal() ([]byte, error) {
 	return ret.Bytes(), err
 }
 
-func (c *Cosmos) handleGetVersion(_ []byte) (response []byte, code [2]byte, err error) {
+func (c *Cosmos) handleGetVersion() (response []byte, code [2]byte, err error) {
 	resp, err := getVersionResponse{
-		TestMode: 1,
+		TestMode: 0,
 		Version: version{
-			Major: 42,
-			Minor: 42,
-			Patch: 42,
+			Major: 2,
+			Minor: 0,
+			Patch: 0,
 		},
 		DeviceLocked: 0,
 	}.Marshal()
@@ -100,6 +110,73 @@ func (c *Cosmos) handleSignSecp256K1(data []byte) (response []byte, code [2]byte
 	return nil, [2]byte{}, nil
 }
 
+type getAddressResponse struct {
+	PublicKey [33]byte
+	Address   [65]byte
+}
+
+func (g getAddressResponse) Marshal() ([]byte, error) {
+	ret := &bytes.Buffer{}
+
+	err := binary.Write(ret, binary.BigEndian, g)
+
+	return ret.Bytes(), err
+}
+
+type getAddressRequest struct {
+	_             [2]byte // space at the beginning, CLA and INS are not interesting here
+	P1            uint8
+	P2            uint8
+	PayloadLength uint8
+	HRPLength     uint8
+}
+
 func (c *Cosmos) handleGetAddrSecp256K1(data []byte) (response []byte, code [2]byte, err error) {
-	return nil, [2]byte{}, nil
+	req := getAddressRequest{}
+	if err := binary.Read(bytes.NewReader(data), binary.BigEndian, &req); err != nil {
+		return nil, commandErrEmptyBuffer, err
+	}
+
+	if req.PayloadLength == 0 {
+		return nil, commandCodeOK, fmt.Errorf("command packet is empty")
+	}
+
+	displayAddrOnDevice := false
+	if req.P1 == 0x01 {
+		displayAddrOnDevice = true
+	}
+
+	log.Println("display on device:", displayAddrOnDevice)
+
+	data = data[6:req.PayloadLength]
+
+	log.Println("data:", data)
+	hrp := data[0:req.HRPLength]
+
+	log.Println("requested hrp:", string(hrp))
+
+	/*addr, err := bech32.Encode("cosmos", testBytes())
+	if err != nil {
+		return nil, [2]byte{0x64, 0x00}, err
+	}*/
+
+	gar := getAddressResponse{}
+
+	d, _ := hex.DecodeString("02963020258b9fae259da3ba669b29d06a165e319eba845f8857859a140426614e")
+
+	copy(gar.Address[:], []byte("cosmos1n99upe3x9ak39x4tnygswnvrdesv828cnrmm3v"))
+	copy(gar.PublicKey[:], d)
+
+	r := &bytes.Buffer{}
+	r.Write(d)
+	r.WriteString("cosmos1n99upe3x9ak39x4tnygswnvrdesv828cnrmm3v")
+	resp, err := gar.Marshal()
+	if err != nil {
+		return nil, [2]byte{0x64, 0x00}, err
+	}
+
+	_ = resp
+
+	// TODO: handle derivation path
+	return r.Bytes(), commandCodeOK, nil
 }
