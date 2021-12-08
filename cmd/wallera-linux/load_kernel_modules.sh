47df4a49ab855398ebb2740cfe7898a3e2ed01a3
Gianguido Sorà <me@gsora.xyz>
2021-12-08 17:00:52 +0100

    chore(wallera-linux): add script to load modules

---
+++ cmd/wallera-linux/load_kernel_modules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+    echo this script needs root privileges
+    exit 1
+fi
+
+set -e
+
+MODULES=(libcomposite configfs dummy_hcd)
+
+for i in ${MODULES[@]}; do
+    echo loading $i
+    modprobe $i
+done
+
+chown $USER /dev/hidg0
