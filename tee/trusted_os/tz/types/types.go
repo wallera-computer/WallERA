c241da575794ed98eb9cd1c869fc8e0afe7a4f99
Gianguido Sorà <me@gsora.xyz>
2021-12-16 20:41:58 +0100

    feature(tz): implement method to easily obtain bytes from Payload on Mail

--- tee/trusted_os/tz/types/types.go
+++ tee/trusted_os/tz/types/types.go
@@ -1,8 +1,27 @@
 package types
 
+import (
+	"encoding/base64"
+	"fmt"
+)
+
 type Mail struct {
 	AppID   uint
 	Payload interface{}
+}
+
+func (m Mail) PayloadBytes() []byte {
+	b, ok := m.Payload.(string)
+	if !ok {
+		panic(fmt.Errorf("payload is not bytes"))
+	}
+
+	data, err := base64.StdEncoding.DecodeString(b)
+	if err != nil {
+		panic(fmt.Errorf("cannot unmarshal base64, %w", err))
+	}
+
+	return data
 }
 
 func (m *Mail) CopyFrom(o Mail) {
